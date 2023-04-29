384de536cd471b55cb66861a6db1386b82bb860e
nafets33 <stapinski89gmail.com>
2023-04-29 14:33:22 -0400

    testing grid

--- fastAPI-server/src/app/database/queen/queries.py
+++ fastAPI-server/src/app/database/queen/queries.py
@@ -70,43 +70,44 @@ def load_queen_pkl(username, prod):
   return queen_pkl
 
 def get_queen_orders_json(username, prod):
-  queen_db = load_queen_pkl(username, prod)
+  # queen_db = load_queen_pkl(username, prod)
 
-  qo = queen_db['queen_orders']
-  col_view = [
-              "honey",
-              "$honey",
-              "symbol",
-              "ticker_time_frame",
-              "trigname",
-              "datetime",
-              "honey_time_in_profit",
-              "filled_qty",
-              "qty_available",
-              "filled_avg_price",
-              "limit_price",
-              "cost_basis",
-              "wave_amo",
-              "status_q",
-              "client_order_id",
-              "origin_wave",
-              "wave_at_creation",
-              "power_up",
-              "sell_reason",
-              "exit_order_link",
-              "queen_order_state",
-              "order_rules",
-              "order_trig_sell_stop",
-              "side",
-          ]
-  df = pd.DataFrame(qo)
+  # qo = queen_db['queen_orders']
+  # col_view = [
+  #             "honey",
+  #             "$honey",
+  #             "symbol",
+  #             "ticker_time_frame",
+  #             "trigname",
+  #             "datetime",
+  #             "honey_time_in_profit",
+  #             "filled_qty",
+  #             "qty_available",
+  #             "filled_avg_price",
+  #             "limit_price",
+  #             "cost_basis",
+  #             "wave_amo",
+  #             "status_q",
+  #             "client_order_id",
+  #             "origin_wave",
+  #             "wave_at_creation",
+  #             "power_up",
+  #             "sell_reason",
+  #             "exit_order_link",
+  #             "queen_order_state",
+  #             "order_rules",
+  #             "order_trig_sell_stop",
+  #             "side",
+  #         ]
+  # df = pd.DataFrame(qo)
   
-  df["$honey"] = pd.to_numeric(df["$honey"], errors='coerce')
-  df["honey"] = pd.to_numeric(df["honey"], errors='coerce')
-  df["honey"] = round(df["honey"] * 100,2)
-  df["$honey"] = round(df["$honey"],0)
+  # df["$honey"] = pd.to_numeric(df["$honey"], errors='coerce')
+  # df["honey"] = pd.to_numeric(df["honey"], errors='coerce')
+  # df["honey"] = round(df["honey"] * 100,2)
+  # df["$honey"] = round(df["$honey"],0)
   
-  df = df[df['queen_order_state'].isin(['running'])]
-  df = df[col_view]
+  # df = df[df['queen_order_state'].isin(['running'])]
+  # df = df[col_view]
+  df = pd.DataFrame([{'a': 2}, {'a':3}])
   json_data = df.to_json(orient='records')
   return json_data
