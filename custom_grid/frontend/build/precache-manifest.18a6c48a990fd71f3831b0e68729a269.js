3485124044c9f3af28912d3d3f24dd1abe437134
sven0227 <sven0227@gmail.com>
2023-05-03 00:17:43 +0300

    update_api_url&remove-btn&datetimeformat

---
+++ custom_grid/frontend/build/precache-manifest.18a6c48a990fd71f3831b0e68729a269.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf780469e3af9e85336b6c2236bb712",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9dfc516c1b398d51b58e",
+    "url": "./static/css/2.f25ae857.chunk.css"
+  },
+  {
+    "revision": "e7a107e35e007b34eba9",
+    "url": "./static/css/main.4a7c0ab3.chunk.css"
+  },
+  {
+    "revision": "9dfc516c1b398d51b58e",
+    "url": "./static/js/2.2555af25.chunk.js"
+  },
+  {
+    "revision": "4fce7e80c956707cb74d1947721c84cf",
+    "url": "./static/js/2.2555af25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a107e35e007b34eba9",
+    "url": "./static/js/main.fc65de2a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.fc65de2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be99a8b073eb36846b2d",
+    "url": "./static/js/runtime-main.1f7e4d05.js"
+  }
+]);
