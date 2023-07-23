bea2813e5063dc3cd5c7f2e69c2642f6e2dc901d
sven0227 <sven0227@gmail.com>
2023-07-24 01:27:20 +0900

    fixed issue for boolean

---
+++ custom_grid/frontend/build/precache-manifest.c64fe677ec97a6d58681af1a4f70db20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5429e408ec74c99c36c05ee8902aba0e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "161bb3567ee4c4dc76f0",
+    "url": "./static/css/2.a79e5ea6.chunk.css"
+  },
+  {
+    "revision": "9edb9d52e09129144192",
+    "url": "./static/css/main.c48466a7.chunk.css"
+  },
+  {
+    "revision": "161bb3567ee4c4dc76f0",
+    "url": "./static/js/2.6f689a1b.chunk.js"
+  },
+  {
+    "revision": "efdc3c4f15e7a6da3b6a2e9768f7cc8c",
+    "url": "./static/js/2.6f689a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9edb9d52e09129144192",
+    "url": "./static/js/main.c75c008e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.c75c008e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be99a8b073eb36846b2d",
+    "url": "./static/js/runtime-main.1f7e4d05.js"
+  }
+]);
