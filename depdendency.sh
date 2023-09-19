426a53768c83eaea70e5ade6eaba390e6087eeef
Clark Alesna <clark_alesna@hotmail.com>
2023-09-19 15:22:15 -0400

    delete

--- depdendency.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# PYTHON 3.10 recommended
-sudo apt install liblzma-dev libsqlite3-dev bzip2 libncurses-dev
-sudo apt install libbz2-dev
-sudo apt install libreadline-devx
-sudo apt-get install gfortran
-sudo apt install cmake libopenblas-dev
-sudo apt install pkg-config
