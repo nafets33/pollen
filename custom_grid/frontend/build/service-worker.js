4130e496b818ddb8be4c8fbc539f363ce432ae2e
nafets33 <stapinski89gmail.com>
2024-04-01 16:17:03 -0400

    grid and upkeep

--- custom_grid/frontend/build/service-worker.js
+++ custom_grid/frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c407fccc33a96aee92b6c2ca58c704d3.js"
+  "./precache-manifest.65a52d1a32691b52cfd6deccab1f46c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
