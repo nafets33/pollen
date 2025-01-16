5bab7ee8c734895b0bdb7b89e335d455fa23f609
nafets33 <stapinski89@gmail.com>
2025-01-15 23:15:57 -0500

    server ready?

---
+++ custom_grid/frontend/build/precache-manifest.40951de957799fd5b8a83c8f31302560.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ee7371992f8c0c91a9b6c1cbe94016",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71140a8ca21fe8efc644",
+    "url": "./static/css/2.739f1b7f.chunk.css"
+  },
+  {
+    "revision": "51789721e577040d589c",
+    "url": "./static/css/main.f21707b1.chunk.css"
+  },
+  {
+    "revision": "71140a8ca21fe8efc644",
+    "url": "./static/js/2.6bc4bf90.chunk.js"
+  },
+  {
+    "revision": "d91272289fe6bde52047c4871a2902b0",
+    "url": "./static/js/2.6bc4bf90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51789721e577040d589c",
+    "url": "./static/js/main.3422a096.chunk.js"
+  },
+  {
+    "revision": "be99a8b073eb36846b2d",
+    "url": "./static/js/runtime-main.1f7e4d05.js"
+  }
+]);
