2fa2923bc619f419b650c4d40462f667b969576f
nafets33 <stapinski89@gmail.com>
2025-03-04 21:56:06 -0500

    Prep work for bigger days

---
+++ custom_grid/frontend/build/precache-manifest.9e5f47cbf52658cc161c3d0680018858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e684cd9f7562bc2699c320273406d60a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "012fa6fd341dee53852e",
+    "url": "./static/css/2.739f1b7f.chunk.css"
+  },
+  {
+    "revision": "2e8afb9f2a4c6c10ff69",
+    "url": "./static/css/main.f21707b1.chunk.css"
+  },
+  {
+    "revision": "012fa6fd341dee53852e",
+    "url": "./static/js/2.efb34210.chunk.js"
+  },
+  {
+    "revision": "d91272289fe6bde52047c4871a2902b0",
+    "url": "./static/js/2.efb34210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e8afb9f2a4c6c10ff69",
+    "url": "./static/js/main.a3ecfbe2.chunk.js"
+  },
+  {
+    "revision": "be99a8b073eb36846b2d",
+    "url": "./static/js/runtime-main.1f7e4d05.js"
+  }
+]);
