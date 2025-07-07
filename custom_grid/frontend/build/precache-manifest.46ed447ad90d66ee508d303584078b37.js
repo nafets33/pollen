cb5740348c048fd6376c47697348e6f0e3eafbcc
nafets33 <stapinski89@gmail.com>
2025-07-06 22:58:32 -0400

    sandbox ready - grid ready - orders via postgres - AI assistant

---
+++ custom_grid/frontend/build/precache-manifest.46ed447ad90d66ee508d303584078b37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec970ef9a58b69390073587fe849c0b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "284be5cd9396495e9639",
+    "url": "./static/css/2.739f1b7f.chunk.css"
+  },
+  {
+    "revision": "8e7d62bbb02cae7e3d44",
+    "url": "./static/css/main.78b9e2ef.chunk.css"
+  },
+  {
+    "revision": "284be5cd9396495e9639",
+    "url": "./static/js/2.24ab0434.chunk.js"
+  },
+  {
+    "revision": "d91272289fe6bde52047c4871a2902b0",
+    "url": "./static/js/2.24ab0434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e7d62bbb02cae7e3d44",
+    "url": "./static/js/main.a4f84610.chunk.js"
+  },
+  {
+    "revision": "be99a8b073eb36846b2d",
+    "url": "./static/js/runtime-main.1f7e4d05.js"
+  }
+]);
