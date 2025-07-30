c3c2ae339c4492c0faa9074afe2c9ccd574cafbf
nafets33 <stapinski89@gmail.com>
2025-07-30 09:41:57 -0400

    demo updates

---
+++ custom_voiceGPT/frontend/build/precache-manifest.7098d59459483414fcf445aa6d606716.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca75733ad0aba7725fbaff4d0ebe5496",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a594cd87c077d62603b0",
+    "url": "./static/css/main.934f655c.chunk.css"
+  },
+  {
+    "revision": "7eca54f7a98a8caa1d16",
+    "url": "./static/js/2.b70bc572.chunk.js"
+  },
+  {
+    "revision": "a594cd87c077d62603b0",
+    "url": "./static/js/main.7caec348.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
